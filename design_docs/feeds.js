c8672a19bd3fdc8e0a8a2f22197062bb17e311e5
King Chung Huang <kchuang@ucalgary.ca>
2015-02-21 16:36:19 -0700

    Add view for analyzed articles
    Keyed by feed and date.

--- design_docs/feeds.js
+++ design_docs/feeds.js
@@ -1,10 +1,12 @@
 {
    "_id": "_design/feeds",
-   "_rev": "1-558a517a8ecb9e7a86fcd53350cd53cb",
    "language": "javascript",
    "views": {
        "all_feeds": {
            "map": "function(doc) {\n\tif (doc.type == 'feed') {\n\t\temit(doc.active, 1);\n\t}\n}"
+       },
+       "analyzed_articles_by_feed_and_date": {
+           "map": "function(doc) {\n\tif (doc.type == 'article' && doc.analyzed == true) {\n\t\temit([doc.feed_id, doc.published], doc.analyzed);\n\t}\n}"
        }
    }
 }
